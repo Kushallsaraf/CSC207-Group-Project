29093de359dd9f304778b80f7bd3063c96d82d63
kushallsaraf <kushallsaraf@gmail.com>
2025-07-17 20:28:23 -0400

    Renamed package from com.kushallsaraf to com.csc207.group

--- src/main/java/com/csc207/group/Main.java
+++ src/main/java/com/csc207/group/Main.java
@@ -1,4 +1,4 @@
-package com.kushallsaraf;
+package com.csc207.group;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
