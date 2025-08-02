57bb0695b542cb23368b5dcb1dc4d621d9a38a88
Taabish Khan <vuvuzela@Anduril.local>
2025-08-01 22:14:19 -0400

    firebase integrated, caching system simplified, established APICache interface

--- src/main/java/Cache/FilePaths.java
+++
@@ -1,6 +0,0 @@
-package Cache;
-
-public class FilePaths {
-
-    public static final String TAABISH_PATH = "src/main/java/Cache/igdb_requests_cache.json";
-}
---
+++ src/main/java/Cache/FirebaseRAWGCache.java
@@ -0,0 +1,7 @@
+package Cache;
+
+public class FirebaseRAWGCache extends FirebaseAPICache{
+    public FirebaseRAWGCache() {
+        super("API/RAWG");
+    }
+}
