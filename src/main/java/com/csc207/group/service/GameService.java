f78f66425bf124df2f9e37e61dd4af4fb9e38058
kushallsaraf <kushallsaraf@gmail.com>
2025-08-04 19:29:45 -0400

    added 3 functions for the backend. These will get the data from the api and give it to the frontend. This is related to my user story about the wishlist for which I am provideing the backend data to the frontend team.

--- src/main/java/com/csc207/group/service/GameService.java
+++ src/main/java/com/csc207/group/service/GameService.java
@@ -19,7 +19,7 @@ public class GameService {
 
         if (array.isEmpty()) return -1;
 
-        JSONObject firstMatch = array.getJSONObject(1);
+        JSONObject firstMatch = array.getJSONObject(0);
         return firstMatch.getInt("id");
     }
 
@@ -41,24 +41,96 @@ public class GameService {
             }
         }
 
-        StringBuilder developer = new StringBuilder("Developer ID(s): ");
+        List<String> developers = new ArrayList<>();
         if (gameJson.has("involved_companies")) {
             JSONArray devArray = gameJson.getJSONArray("involved_companies");
             for (int i = 0; i < devArray.length(); i++) {
-                developer.append(devArray.getInt(i)).append(" ");
+                developers.add(getDeveloperById(devArray.getInt(i)));
             }
         }
+        else {
+            developers.add("Unknown");
+        }
 
-        double aggregate_rating = gameJson.optDouble("rating", 0);
-        double rating_count = gameJson.optDouble("rating_count", 0);
+        double aggregate_rating = gameJson.optDouble("total_rating", 0);
+        double rating_count = gameJson.optDouble("total_rating_count", 0);
+
+        List<String> platforms = new ArrayList<>();
+        if (gameJson.has("platforms")) {
+            JSONArray platformArray = gameJson.getJSONArray("platforms");
+            for (int i = 0; i < platformArray.length(); i++) {
+                platforms.add(getPlatformById(platformArray.getInt(i)));
+            }
+        }
+        else {
+            platforms.add("Unknown");
+        }
+
+        String cover_page = "";
+        if (gameJson.has("cover")) {
+            cover_page = getCoverPageById(gameJson.optInt("cover", 0));
+        }
+        else {
+            cover_page = "No cover page";
+        }
+
+        String rating = "";
+        if (gameJson.has("age_ratings")) {
+            JSONArray ageRatingArray = gameJson.getJSONArray("age_ratings");
+            rating = getAgeRatingById(ageRatingArray.getInt(0));
+        }
+        else {
+            rating = "age rating not found";
+        }
+
+
+        String release_date = "";
+        if (gameJson.has("release_dates")) {
+            JSONArray releaseDateArray = gameJson.getJSONArray("release_dates");
+            release_date = getRelease_date(releaseDateArray.getInt(0));
+        }
+        else {
+            release_date = "Release date not found";
+        }
+
+
 
         Game gameobj = new Game();
         gameobj.setName(name);
         gameobj.setGenres(genres);
-        gameobj.setDeveloper(developer.toString());
+        gameobj.setDeveloper(developers);
         gameobj.setCritic_rating(aggregate_rating);
         gameobj.setRating_count(rating_count);
+        gameobj.setPlatforms(platforms);
+        gameobj.setCover_image(cover_page);
+        gameobj.setAge_rating(rating);
+        gameobj.setRelease_date(release_date);
 
         return gameobj;
+    }
+
+    public String getDeveloperById(int id) {
+        JsonNode response = apiClient.getDevelopersById(id);
+        return response.getArray().getJSONObject(0).getString("name");
+    }
+
+    public String getPlatformById(int id) {
+        JsonNode response = apiClient.getPlatformsById(id);
+        return response.getArray().getJSONObject(0).getString("name");
+    }
+
+    public String getCoverPageById(int id) {
+        JsonNode response = apiClient.getCoverArtById(id);
+        return response.getArray().getJSONObject(0).getString("url");
+    }
+
+    public String getAgeRatingById(int id) {
+        JsonNode response = apiClient.getAgeRatingById(id);
+        return response.getArray().getJSONObject(0).getString("rating");
+    }
+
+    public String getRelease_date(int id) {
+        JsonNode response = apiClient.getReleaseDateById(id);
+        return response.getArray().getJSONObject(0).getString("human");
     }
 }
