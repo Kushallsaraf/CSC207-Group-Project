f552588a332f173024cafabceed729779791f2d0
unknown <yashingale0508@gmail.com>
2025-08-05 19:18:53 -0400

    Review Front End Done

--- src/main/java/ReviewView.java
+++ src/main/java/ReviewView.java
@@ -2,10 +2,9 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -15,7 +14,7 @@ public class ReviewView extends Application {
 
     public void start(Stage stage) {
         VBox vbox = new VBox(10);
-        vbox.setStyle("-fx-padding: 20; -fx-background-color: #222;");
+        vbox.setStyle("-fx-padding: 5; -fx-background-color: #222;");
         vbox.setAlignment(Pos.TOP_CENTER);
 
         Label title = new Label("Game Reviews");
@@ -47,6 +46,7 @@ public class ReviewView extends Application {
         reviews.setVgap(10);
         reviews.setHgap(10);
         reviews.setStyle("-fx-background-color: #222");
+        reviews.setAlignment(Pos.TOP_CENTER);
 
         Integer i = 0;
         for (Review review : gameReviews.getReviews()) {
@@ -57,16 +57,39 @@ public class ReviewView extends Application {
         ScrollPane curreviews = new ScrollPane(reviews);
         curreviews.setFitToWidth(true);
         curreviews.setPadding(new Insets(20));
-        curreviews.setPrefHeight(600);
+        curreviews.setPrefHeight(750);
         curreviews.setStyle("-fx-background-color: #222");
 
-        TextField leave = new TextField();
+        TextArea leave = new TextArea();
         leave.setPromptText("Tell what you think!");
+        TextFormatter<String> formatter = new TextFormatter<>(change -> {
+            if (change.getControlNewText().length() <= 300) {
+                return change;
+            } else {
+                return null;
+            }
+        });
+        leave.setTextFormatter(formatter);
+        leave.setMinWidth(1600);
+        leave.setMinHeight(60);
+        leave.setStyle("-fx-font-size: 14");
 
-        vbox.getChildren().addAll(title,subtitle,curreviews);
-        Scene scene = new Scene(vbox,1600,900);
+        Button post = new Button("Post");
+        post.setStyle("-fx-background-color: #222; -fx-font-size: 16; -fx-background-radius: 10; -fx-border-radius: 10; -fx-text-fill: white; -fx-border-color: white; -fx-border-width: 4");
+
+        HBox postsys = new HBox(25);
+        postsys.setPrefSize(1750,80);
+        postsys.setAlignment(Pos.CENTER);
+        postsys.getChildren().addAll(leave,post);
+
+        vbox.getChildren().addAll(title,subtitle,curreviews,postsys);
+        Scene scene = new Scene(vbox,1920,1000);
         stage.setScene(scene);
         stage.show();
+
+        post.setOnAction(e -> {
+            gameReviews.addReview(new Review("newuser",leave.getText(), 4.5));
+        });
     }
 
     public static void main(String[] args) {
