0c14d2b3007746abb89b45a506cad18e355f82d1
kushallsaraf <kushallsaraf@gmail.com>
2025-08-05 18:42:58 -0400

    created a service class to handle DLC data and a DLC class which the frontend will use to populate their fields. This is based on one of the user story's backend which I am working on

---
+++ src/main/java/com/csc207/group/service/DLCService.java
@@ -0,0 +1,35 @@
+package com.csc207.group.service;
+
+import Cache.IGDBFirebaseAPICache;
+import com.csc207.group.data_access.IGDBApiClient;
+import com.csc207.group.model.DLC;
+import kong.unirest.JsonNode;
+import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONObject;
+
+public class DLCService {
+    private final IGDBApiClient apiClient = new IGDBApiClient(new IGDBFirebaseAPICache());
+
+    public DLC getDLCById(int id) {
+        JsonNode json = apiClient.getGameDetailsById(id);
+        JSONArray array = json.getArray();
+
+        // Setting name
+        JSONObject obj = array.getJSONObject(0);
+        String name = obj.optString("name", "None");
+
+        // Setting description
+        String description = obj.optString("summary", "None");
+
+        // Setting cover image url
+        String cover_page = "";
+        if (obj.has("cover")) {
+            cover_page = new GameService().getCoverPageById(obj.optInt("cover", 0));
+        }
+        else {
+            cover_page = "No cover page";
+        }
+
+        return new DLC(name, description, cover_page);
+    }
+}
