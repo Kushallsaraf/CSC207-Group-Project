96fc29e56e42f0415e3719fc31137fd71345f40c
unknown <yashingale0508@gmail.com>
2025-08-07 20:44:56 -0400

    Rating input

--- src/main/java/ReviewView.java
+++ src/main/java/ReviewView.java
@@ -57,7 +57,7 @@ public class ReviewView extends Application {
         ScrollPane curreviews = new ScrollPane(reviews);
         curreviews.setFitToWidth(true);
         curreviews.setPadding(new Insets(20));
-        curreviews.setPrefHeight(750);
+        curreviews.setMaxHeight(680);
         curreviews.setStyle("-fx-background-color: #222");
 
         TextArea leave = new TextArea();
@@ -70,9 +70,12 @@ public class ReviewView extends Application {
             }
         });
         leave.setTextFormatter(formatter);
-        leave.setMinWidth(1600);
+        leave.setMinWidth(1200);
         leave.setMinHeight(60);
         leave.setStyle("-fx-font-size: 14");
+
+        TextField rating = new TextField();
+        rating.setPromptText("Rating");
 
         Button post = new Button("Post");
         post.setStyle("-fx-background-color: #222; -fx-font-size: 16; -fx-background-radius: 10; -fx-border-radius: 10; -fx-text-fill: white; -fx-border-color: white; -fx-border-width: 4");
@@ -80,7 +83,7 @@ public class ReviewView extends Application {
         HBox postsys = new HBox(25);
         postsys.setPrefSize(1750,80);
         postsys.setAlignment(Pos.CENTER);
-        postsys.getChildren().addAll(leave,post);
+        postsys.getChildren().addAll(leave,rating,post);
 
         vbox.getChildren().addAll(title,subtitle,curreviews,postsys);
         Scene scene = new Scene(vbox,1920,1000);
@@ -88,7 +91,32 @@ public class ReviewView extends Application {
         stage.show();
 
         post.setOnAction(e -> {
-            gameReviews.addReview(new Review("newuser",leave.getText(), 4.5));
+            if (rating.getText().isEmpty()) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Invalid Input");
+                alert.setHeaderText(null);
+                alert.setContentText("Please enter a valid rating 0-5");
+                alert.showAndWait();
+            }
+
+            try {
+                double value = Double.parseDouble(rating.getText());
+                if (value < 0 || value > 5) {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Invalid Input");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Please enter a valid rating 0-5");
+                    alert.showAndWait();
+                } else {
+                    gameReviews.addReview(new Review("newuser",leave.getText(), 4.5));
+                }
+            } catch (NumberFormatException f) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Invalid Input");
+                alert.setHeaderText(null);
+                alert.setContentText("Please enter a valid rating 0-5");
+                alert.showAndWait();
+            }
         });
     }
 
